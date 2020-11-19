1373b423ff846942e3599180cff360f0628a6865
shangxq <shangxq@guahao.com>
2020-11-19 11:18:15 +0800

    style: 增加一些注释

--- src/chapter2/Problem07.java
+++ src/chapter2/Problem07.java
@@ -15,15 +15,29 @@ public class Problem07 {
     private Map<Integer,Integer> map;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         this.preorder = preorder;
+        //方便我们O1时间找到root 中序下标
         map = new HashMap<>(inorder.length);
         for (int i=0;i<inorder.length;i++) map.put(inorder[i],i);
         return buildTree(0,0,preorder.length-1);
     }
+
+    /**
+     *
+     * @param rootIndexForPre 前序遍历根节点下标
+     * @param startIndexForIn 先序遍历左边界
+     * @param endIndexForIn 先序遍历右边界
+     * @return 根节点
+     */
     private TreeNode buildTree(int rootIndexForPre,int startIndexForIn,int endIndexForIn){
+        //递归终止
         if(startIndexForIn>endIndexForIn) return null;
+        //构建根节点
         TreeNode root = new TreeNode(preorder[rootIndexForPre]);
+        //找到根节点的中序下标
         int rootIndexForIn = map.get(preorder[rootIndexForPre]);
+        //构建左子树 左侧新根=root+1;左侧先序边界=[原左边界,root先序-1]
         root.setLeft(buildTree(rootIndexForPre+1,startIndexForIn,rootIndexForIn-1));
+        //构建右子树 右侧新根=root+左个数+1;左个数=root先序-原左边界;右侧先序边界=[root先序+1,原右边界]
         root.setRight(buildTree(rootIndexForIn-startIndexForIn+rootIndexForPre+1,rootIndexForIn+1,endIndexForIn));
         return root;
     }
